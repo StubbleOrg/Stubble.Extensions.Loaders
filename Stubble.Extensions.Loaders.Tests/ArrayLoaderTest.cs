d7d14861710cfbeb4de026a93ab598ebb852ad7e
Romanx <mcauliffe.alex@gmail.com>
2015-07-23 20:26:05 +0100

    Added tests for ArrayLoader

---
+++ Stubble.Extensions.Loaders.Tests/ArrayLoaderTest.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using Xunit;
+
+namespace Stubble.Extensions.Loaders.Tests
+{
+    public class ArrayLoaderTest
+    {
+        [Fact]
+        public void It_Should_Take_An_Array_Of_Templates()
+        {
+            var loader = new ArrayLoader(new Dictionary<string, string>
+            {
+                { "Foo", "I'm {{Foo}}" },
+                { "Bar", "I'm {{Bar}}" }
+            });
+
+            Assert.Equal(2, loader.TemplateCache.Count);
+        }
+
+        [Fact]
+        public void It_Should_Create_A_Clone_Of_Passed_Values()
+        {
+            var arr = new Dictionary<string, string>
+            {
+                {"Foo", "I'm {{Foo}}"},
+                {"Bar", "I'm {{Bar}}"}
+            };
+            var loader = new ArrayLoader(arr);
+            Assert.Equal(2, loader.TemplateCache.Count);
+            arr.Add("FooBar", "{{Foo}}{{Bar}}");
+            Assert.Equal(2, loader.TemplateCache.Count);
+        }
+
+        [Fact]
+        public void It_Should_Load_Items_From_Template_If_Exists()
+        {
+            var loader = new ArrayLoader(new Dictionary<string, string>
+            {
+                { "Foo", "I'm {{Foo}}" },
+                { "Bar", "I'm {{Bar}}" }
+            });
+
+            Assert.Equal("I'm {{Foo}}", loader.Load("Foo"));
+            Assert.Null(loader.Load("Foo2"));
+        }
+    }
+}
--- Stubble.Extensions.Loaders/ArrayLoader.cs
+++ Stubble.Extensions.Loaders/ArrayLoader.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
 using Stubble.Core.Interfaces;
 
 namespace Stubble.Extensions.Loaders

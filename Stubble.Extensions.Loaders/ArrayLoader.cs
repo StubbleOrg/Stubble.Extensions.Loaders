62337bbc20bd1b1458e5276d61552b5fa3dd93f5
Romanx <mcauliffe.alex@gmail.com>
2015-07-23 20:19:09 +0100

    Added ArrayLoader with initial implementation

---
+++ Stubble.Extensions.Loaders/ArrayLoader.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Stubble.Core.Interfaces;
+
+namespace Stubble.Extensions.Loaders
+{
+    public class ArrayLoader : IStubbleLoader
+    {
+        internal readonly IDictionary<string, string> TemplateCache;
+
+        public ArrayLoader(IDictionary<string, string> templates)
+        {
+            TemplateCache = new Dictionary<string, string>(templates);
+        }
+
+        public string Load(string name)
+        {
+            return TemplateCache.ContainsKey(name) ? TemplateCache[name] : null;
+        }
+    }
+}

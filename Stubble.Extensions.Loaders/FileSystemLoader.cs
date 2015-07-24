0be1e644d5d113ef4b114b14f8f144f2687526e4
Romanx <mcauliffe.alex@gmail.com>
2015-07-24 10:23:15 +0100

    Added internals for FileSystemLoader
    includes default extension of .mustache

--- Stubble.Extensions.Loaders/FileSystemLoader.cs
+++ Stubble.Extensions.Loaders/FileSystemLoader.cs
@@ -3,10 +3,30 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Stubble.Core.Interfaces;
 
 namespace Stubble.Extensions.Loaders
 {
-    public class FileSystemLoader
+    public class FileSystemLoader : IStubbleLoader
     {
+        internal const string DefaultFileType = "mustache";
+        private readonly string _path;
+        private readonly string _fileType;
+        internal IDictionary<string, string> TemplateCache = new Dictionary<string, string>(10); 
+
+        public FileSystemLoader(string path) : this(path, DefaultFileType)
+        {
+        }
+
+        public FileSystemLoader(string path, string fileType)
+        {
+            _path = path;
+            _fileType = fileType;
+        }
+
+        public string Load(string name)
+        {
+            throw new NotImplementedException();
+        }
     }
 }

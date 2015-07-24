d27ce0dbd92275f79f4566fedcc7448b92af3e58
Romanx <mcauliffe.alex@gmail.com>
2015-07-24 10:40:16 +0100

    Added simple path tests for FileSystemLoader

---
+++ Stubble.Extensions.Loaders.Tests/FileSystemLoaderTest.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Stubble.Extensions.Loaders.Tests
+{
+    public class FileSystemLoaderTest
+    {
+        [Fact]
+        public void It_Should_Take_Path_As_Argument()
+        {
+            var loader  = new FileSystemLoader("./templates");
+            Assert.Equal("./templates", loader.Path);
+        }
+
+        [Fact]
+        public void It_Should_Strip_Possible_Ending_Slash_On_Path()
+        {
+            var loader = new FileSystemLoader("./templates/");
+            Assert.Equal("./templates", loader.Path);
+        }
+    }
+}

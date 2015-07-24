158baad1535c3dd2eccacad90abc5d48580329a0
Romanx <mcauliffe.alex@gmail.com>
2015-07-24 10:32:53 +0100

    Added inital load implementation

--- Stubble.Extensions.Loaders/FileSystemLoader.cs
+++ Stubble.Extensions.Loaders/FileSystemLoader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,22 +12,29 @@ namespace Stubble.Extensions.Loaders
     {
         internal const string DefaultFileType = "mustache";
         private readonly string _path;
-        private readonly string _fileType;
+        private readonly string _extension;
         internal IDictionary<string, string> TemplateCache = new Dictionary<string, string>(10); 
 
         public FileSystemLoader(string path) : this(path, DefaultFileType)
         {
         }
 
-        public FileSystemLoader(string path, string fileType)
+        public FileSystemLoader(string path, string extension)
         {
-            _path = path;
-            _fileType = fileType;
+            _path = path.TrimEnd('/');
+            _extension = extension;
         }
 
         public string Load(string name)
         {
-            throw new NotImplementedException();
+            if (TemplateCache.ContainsKey(name)) return TemplateCache[name];
+
+            var fileName = _path + "/name." + _extension;
+            if (!File.Exists(fileName)) return null;
+
+            var contents = File.ReadAllText(fileName);
+            TemplateCache.Add(name, contents);
+            return contents;
         }
     }
 }

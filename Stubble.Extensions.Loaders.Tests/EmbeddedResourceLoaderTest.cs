de25117c3b5c2181170f646172f82097592ca699
Romanx <mcauliffe.alex@gmail.com>
2015-07-25 17:01:43 +0100

    Added first test of loaded embedded resource

---
+++ Stubble.Extensions.Loaders.Tests/EmbeddedResourceLoaderTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Stubble.Extensions.Loaders.Tests
+{
+    public class EmbeddedResourceLoaderTest
+    {
+        [Fact]
+        public void It_Should_Be_Able_To_Find_EmbeddedResources()
+        {
+            var loader = new EmbeddedResourceLoader();
+            var result = loader.Load("EmbeddedFoo");
+            Assert.Equal("I'm the Embedded {{foo}} template.", result);
+        }
+    }
+}

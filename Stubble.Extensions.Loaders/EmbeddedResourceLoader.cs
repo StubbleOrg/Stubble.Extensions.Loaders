3ca824fa635d476e9d7fa8d012cdb70e0e61e587
Romanx <mcauliffe.alex@gmail.com>
2015-07-25 17:01:27 +0100

    Added inital implmentation for ResourceLoader

--- Stubble.Extensions.Loaders/EmbeddedResourceLoader.cs
+++ Stubble.Extensions.Loaders/EmbeddedResourceLoader.cs
@@ -1,12 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
+using Stubble.Core.Interfaces;
 
 namespace Stubble.Extensions.Loaders
 {
-    public class EmbeddedResourceLoader
+    public class EmbeddedResourceLoader : IStubbleLoader
     {
+        internal const string DefaultFileType = "mustache";
+        internal readonly string Extension;
+        private readonly Assembly _assembly;
+
+        public EmbeddedResourceLoader()
+        {
+            _assembly = Assembly.GetCallingAssembly();
+            Extension = DefaultFileType;
+        }
+
+        public EmbeddedResourceLoader(string extension)
+        {
+            _assembly = Assembly.GetCallingAssembly();
+            Extension = extension;
+        }
+
+        public string Load(string name)
+        {
+            var resourceName = _assembly.GetManifestResourceNames().FirstOrDefault(rn => rn.Contains(name + "." + Extension));
+            if (resourceName == null) return null;
+
+            var stream = _assembly.GetManifestResourceStream(resourceName);
+            if (stream == null) return null;
+            using (var streamReader = new StreamReader(stream, Encoding.UTF8))
+            {
+                return streamReader.ReadToEnd();
+            }
+        }
     }
 }

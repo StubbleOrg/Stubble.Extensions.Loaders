54036a74f878da8c1ed232acd33d4a3c21f319aa
Romanx <mcauliffe.alex@gmail.com>
2016-03-15 10:38:06 +0000

    Add tests for new recursive file searching
    closes #1

--- Stubble.Extensions.Loaders/FileSystemLoader.cs
+++ Stubble.Extensions.Loaders/FileSystemLoader.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Concurrent;
 using System.Collections.Specialized;
 using System.IO;
+using System.Linq;
 using Stubble.Core.Interfaces;
 
 namespace Stubble.Extensions.Loaders
@@ -8,25 +9,50 @@ namespace Stubble.Extensions.Loaders
     public class FileSystemLoader : IStubbleLoader
     {
         internal const string DefaultFileType = "mustache";
+        internal const char DefaultDelimiter = ':';
         internal readonly string Path;
         internal readonly string Extension;
+        internal readonly char Delimiter;
         internal ConcurrentDictionary<string, string> TemplateCache = new ConcurrentDictionary<string, string>();
 
-        public FileSystemLoader(string path) : this(path, DefaultFileType)
+        public FileSystemLoader(string path) : this(path, DefaultDelimiter, DefaultFileType)
         {
         }
 
-        public FileSystemLoader(string path, string extension)
+        public FileSystemLoader(string path, string extension) : this(path, DefaultDelimiter, extension)
+        {
+        }
+
+        public FileSystemLoader(string path, char delimiter) : this(path, delimiter, DefaultFileType)
+        {
+        }
+
+        public FileSystemLoader(string path, char delimiter, string extension)
         {
             Path = path.TrimEnd('/', '\\');
             Extension = extension;
+            Delimiter = delimiter;
         }
 
         public string Load(string name)
         {
             if (TemplateCache.ContainsKey(name)) return TemplateCache[name];
+            var split = name.Split(Delimiter).Select(s => s.Trim()).ToArray();
+            var route = $"{Path}";
+            while (split.Length > 1)
+            {
+                if (Directory.Exists($@"{Path}\{split[0]}"))
+                {
+                    route += $@"\{split[0]}";
+                    split = split.Skip(1).ToArray();
+                }
+                else
+                {
+                    return null;
+                }
+            }
 
-            var fileName = Path + "/" + name + "." + Extension;
+            var fileName = route + "/" + split.First() + "." + Extension;
             if (!File.Exists(fileName)) return null;
             var contents = File.ReadAllText(fileName);
             AddToTemplateCache(name, contents);
